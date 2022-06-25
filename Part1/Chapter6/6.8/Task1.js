a17a91088a403c86a4c3a623db9331e4cb773bab
Сергей Собин <tesla776@mail.ru>
2022-06-26 00:05:36 +0400

    Задание 1

---
+++ Part1/Chapter6/6.8/Task1.js
@@ -0,0 +1,29 @@
+"use strict";
+//setInterval
+function printNumbers(from, to) {
+    let current = from;
+
+    let timerId = setInterval(function () {
+        console.log(current);
+        if (current == to) {
+            clearInterval(timerId);
+        }
+        current++;
+    }, 1000);
+}
+
+printNumbers(5, 10);
+//setTimeout
+function printNumbers(from, to) {
+    let current = from;
+
+    setTimeout(function go() {
+        console.log(current);
+        if (current < to) {
+            setTimeout(go, 1000);
+        }
+        current++;
+    }, 1000);
+}
+
+printNumbers(5, 10);
