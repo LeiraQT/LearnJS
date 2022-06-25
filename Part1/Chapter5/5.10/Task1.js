dac74947d22084fa1f804118713de42530caef3b
Сергей Собин <tesla776@mail.ru>
2022-06-25 21:30:49 +0400

    Криво сохранил

---
+++ Part1/Chapter5/5.10/Task1.js
@@ -0,0 +1,15 @@
+"use strict";
+let user = {
+    name: "John",
+    years: 30
+};
+
+let {
+    name,
+    years: age,
+    isAdmin = false
+} = user;
+
+console.log(name);
+console.log(age); 
+console.log(isAdmin); 
---
+++ Part1/Chapter5/5.9/Task2.js
@@ -0,0 +1,9 @@
+"use strict";
+function count(obj) {
+    return Object.keys(obj).length;
+}
+let user = {
+    name: 'John',
+    age: 30
+};
+alert(count(user)); // 2
