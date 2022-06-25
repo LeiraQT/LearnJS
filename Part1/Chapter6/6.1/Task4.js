6255934160b301dd109a394c76a912c948fe74f4
Сергей Собин <tesla776@mail.ru>
2022-06-25 22:49:40 +0400

    Задание 4-5

--- Part1/Chapter6/6.1/Task4.js
+++ Part1/Chapter6/6.1/Task4.js
@@ -2,34 +2,60 @@
 let list = {
     value: 1,
     next: {
-      value: 2,
-      next: {
-        value: 3,
+        value: 2,
         next: {
-          value: 4,
-          next: null
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
         }
-      }
     }
-  };
-  function printListCyc(list) {
+};
+
+function printListCyc(list) {
     let tmp = list;
-  
+
     while (tmp) {
-      alert(tmp.value);
-      tmp = tmp.next;
+        console.log(tmp.value);
+        tmp = tmp.next;
     }
-  
-  }
-  function printListRec(list) {
 
-    alert(list.value); 
-  
+}
+
+function printReverseListCyc(list) {
+    let arr = [];
+    let tmp = list;
+
+    while (tmp) {
+        arr.push(tmp.value);
+        tmp = tmp.next;
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i]);
+    }
+}
+
+function printListRec(list) {
+
+    console.log(list.value);
+
     if (list.next) {
-      printList(list.next); 
+        printList(list.next);
     }
-  
-  }
 
-  console.log(printListCyc(list));
-  console.log(printListRec(list));
+}
+
+function printReverseListRec(list) {
+
+    if (list.next) {
+        printReverseListRec(list.next);
+    }
+
+    console.log(list.value);
+}
+printReverseListCyc(list);
+printListCyc(list);
+printReverseListRec(list);
+printListRec(list);
