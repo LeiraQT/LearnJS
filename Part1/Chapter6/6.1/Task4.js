423309765fc011e33f8355d2a55c117f3ff20689
Сергей Собин <tesla776@mail.ru>
2022-06-25 22:37:42 +0400

    Задание 4

---
+++ Part1/Chapter6/6.1/Task4.js
@@ -0,0 +1,35 @@
+"use strict";
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  function printListCyc(list) {
+    let tmp = list;
+  
+    while (tmp) {
+      alert(tmp.value);
+      tmp = tmp.next;
+    }
+  
+  }
+  function printListRec(list) {
+
+    alert(list.value); 
+  
+    if (list.next) {
+      printList(list.next); 
+    }
+  
+  }
+
+  console.log(printListCyc(list));
+  console.log(printListRec(list));
