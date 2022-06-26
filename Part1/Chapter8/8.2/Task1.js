84f173a6d74563ba4f5774a308fe6d8ebc3d8bc0
Сергей Собин <tesla776@mail.ru>
2022-06-26 13:52:39 +0400

    8.1(beaut), 8.3

--- Part1/Chapter8/8.2/Task1.js
+++ Part1/Chapter8/8.2/Task1.js
@@ -1,9 +1,10 @@
 "use strict";
+
 function Fruit(name) {
     this.fruit = name;
-  }
-  
-  let fruit = new Fruit('apple');
-  let fruit2 = new fruit.constructor('pineapple');
-  
-  alert( fruit2.fruit );
+}
+
+let fruit = new Fruit('apple');
+let fruit2 = new fruit.constructor('pineapple');
+
+console.log(fruit2.fruit);
---
+++ Part1/Chapter8/8.3/Task1.js
@@ -0,0 +1,25 @@
+"use strict";
+//1
+Function.prototype.defer = function (ms) {
+    setTimeout(this, ms);
+};
+
+function f() {
+    console.log("Hello!");
+}
+
+f.defer(1000);
+//2
+Function.prototype.defer = function (ms) {
+    let f = this;
+    return function (...args) {
+        setTimeout(() => f.apply(this, args), ms);
+    }
+};
+
+// check it
+function f(a, b) {
+    console.log(a + b);
+}
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
