30e4d84845088d3737818424c4616239a48f46f8
Сергей Собин <tesla776@mail.ru>
2022-06-26 12:48:56 +0400

    Задание 4

--- Part1/Chapter6/6.9/Task3.js
+++ Part1/Chapter6/6.9/Task3.js
@@ -1,23 +1,25 @@
 "use strict";
+
 function debounce(f, ms) {
 
-    let isCooldown = false;
-  
-    return function() {
-      if (isCooldown) return;
-  
-      f.apply(this, arguments);
-  
-      isCooldown = true;
-  
-      setTimeout(() => isCooldown = false, ms);
-    };
-  
-  }let f = debounce(alert, 1000);
+  let isCooldown = false;
 
-  f(1); 
-  f(2);
-  
-  setTimeout( () => f(3), 100); 
-  setTimeout( () => f(4), 1100);
-  setTimeout( () => f(5), 1500);
+  return function () {
+    if (isCooldown) return;
+
+    f.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+
+}
+let f = debounce(alert, 1000);
+
+f(1);
+f(2);
+
+setTimeout(() => f(3), 100);
+setTimeout(() => f(4), 1100);
+setTimeout(() => f(5), 1500);
